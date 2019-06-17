643e195796b1255626512026b2756051b6b22986
Aditya Bose <13054902+adbose@users.noreply.github.com>
2019-06-17 19:03:09 +0530

    Fixed response object

--- mood_finder.py
+++ mood_finder.py
@@ -1,5 +1,5 @@
 # importing the requests library for http requests
-import requests
+import requests, json
 
 API_ENDPOINT = 'http://text-processing.com/api/sentiment/'
 
@@ -8,11 +8,16 @@ while not stop:
     text = input('Enter your feelings...\n')
     payload = {'text': text}
     resp = requests.post(API_ENDPOINT, data=payload)
-    response_dict = resp.text
-    # print(response_dict)
+    print('Type of the response: ', type(resp))
+    response_text = resp.text
+    print(response_text)  # this is a JSON string, not a dictionary
+    print('Type of the response: ', type(response_text))
+    response_dict = json.loads(response_text)
+    print('Type of the response: ', type(response_dict))
+
     if response_dict['label'] == 'pos':
         print('The mood is Positive')
-    if response_dict['label'] == 'neg':
+    elif response_dict['label'] == 'neg':
         print('The mood is Negative')
     else:
         print("It's complicated")
